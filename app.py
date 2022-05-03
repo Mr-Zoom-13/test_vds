31b0856d37a332751d98936794ddb7f37eb6b8b1
Mr Zoom <mr.zoom.orsk@gmail.com>
2022-05-03 20:58:39 +0500

    Ready!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
+    serve(app, host='0.0.0.0', port=5000)
